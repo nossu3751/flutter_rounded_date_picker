6ce157a072804467ae621d9e268ce56bd0fe3747
노성 <nossu3751@noseongs-MacBook-Pro.local>
2020-06-01 15:40:17 +0900

    change back

--- lib/src/widgets/flutter_rounded_day_picker.dart
+++ lib/src/widgets/flutter_rounded_day_picker.dart
@@ -402,7 +402,7 @@ class FlutterRoundedDayPicker extends StatelessWidget {
     }
 
     return Padding(
-      padding: style?.paddingDatePicker ?? EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+      padding: style?.paddingDatePicker ?? EdgeInsets.symmetric(horizontal: 8, vertical: 2),
       child: Column(
         children: <Widget>[
           Container(
